482c76fffa1bc0e3a557dd02054eae55c76c3fef
Colin Taylor <5434186+colzilla@users.noreply.github.com>
2019-11-27 13:02:44 -0800

    more sensible names

--- code.py
+++ code.py
@@ -1,30 +1,41 @@
 import board
 import digitalio
 import pulseio
+from adafruit_hid.gamepad import Gamepad
 from time import sleep
 
-from adafruit_hid.gamepad import Gamepad
-
+#create the gampepad object
 gp = Gamepad()
 
-ax = pulseio.PulseIn(board.D3, 5)
-ay = pulseio.PulseIn(board.A4, 5)
+#setup the pulse readers
+ch1 = pulseio.PulseIn(board.A3, 1)
+ch2 = pulseio.PulseIn(board.A4, 1)
 
-axmin = 1500
-axmax = 1500
-aymin = 1500
-aymax = 1500
-
+#used in auto-calibration
+ch1min = 1500
+ch1max = 1500
+ch2min = 1500
+ch2max = 1500
+allowed_min = 900
+allowed_max = 2200
 
 def range_map(x, in_min, in_max, out_min, out_max):
-    x =  (x - in_min) * (out_max - out_min) // (in_max - in_min) + out_min
-    return x
+    joy_value =  (x - in_min) * (out_max - out_min) // (in_max - in_min) + out_min
+    return joy_value
 
 while True:
-    if len(ax) > 0 and ax[0] > 900 and ax[0] < 2100 and len(ay) > 0 and ay[0] > 900 and ay[0] < 2100: 
+    if len(ch1) > 0: 
+        xin = ch1[0] 
+    else 
+        xin = 1500
+    
+    if len(ch2) > 0: 
+        yin = ch2[0] 
+    else 
+        yin = 1500
+        
+    and xin > allowed_min and xin < allowed_max and yin > allowed_min and yin < allowed_max: 
         try:
-            xin = ax[0]
-            yin = ay[0]
             if xin < axmin: axmin = xin
             if xin > axmax and xin < 2200: axmax = xin
             if yin < aymin: aymin = yin
@@ -33,7 +44,7 @@ while True:
             y=range_map(yin, aymin, aymax, -127, 127) * -1
             gp.move_joysticks(x,y)
             print("axmin", axmin, "ax", xin, "axmax", axmax, "aymin", aymin, "ay", yin, "aymax", aymax, "x", x, "y", y)
-            sleep(0.02)
+            sleep(0.01)
         except:
             print("ax", xin, "ay", yin)
             pass
