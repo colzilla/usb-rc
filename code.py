a8bd6b336ec0e6890ab5baaa5f25d4e38a8c8f8a
Colin Taylor <colin.taylor@inmarsat.com>
2019-11-27 14:28:58 -0800

    added debug option.  Change the value for "debug" to True and there are extra debugging lines on the console.

--- code.py
+++ code.py
@@ -3,51 +3,64 @@ import digitalio
 import pulseio
 from adafruit_hid.gamepad import Gamepad
 from time import sleep
+import sys
+
+debug=False
 
 #create the gampepad object
 gp = Gamepad()
 
 #setup the pulse readers
-ch1 = pulseio.PulseIn(board.A3, 1)
-ch2 = pulseio.PulseIn(board.A4, 1)
+ch1 = pulseio.PulseIn(board.A0, 5)
+ch2 = pulseio.PulseIn(board.A1, 5)
 
 #used in auto-calibration
-ch1min = 1500
-ch1max = 1500
-ch2min = 1500
-ch2max = 1500
-allowed_min = 900
+ch1min = 1499
+ch1max = 1501
+ch2min = 1499
+ch2max = 1501
+allowed_min = 800
 allowed_max = 2200
+loop=0
 
 def range_map(x, in_min, in_max, out_min, out_max):
     joy_value =  (x - in_min) * (out_max - out_min) // (in_max - in_min) + out_min
     return joy_value
 
 while True:
+    if(debug): print("\nloop[{}]".format(loop))
+    loop += 1
+
     if len(ch1) > 0: 
-        xin = ch1[0] 
-    else 
-        xin = 1500
+        ch1in = ch1[0] 
+        if(debug): print("ch1 was ", ch1in)
+    else: 
+        if(debug): print("ch1 was empty")
+        ch1in = 1500
     
     if len(ch2) > 0: 
-        yin = ch2[0] 
-    else 
-        yin = 1500
-        
-    and xin > allowed_min and xin < allowed_max and yin > allowed_min and yin < allowed_max: 
-        try:
-            if xin < axmin: axmin = xin
-            if xin > axmax and xin < 2200: axmax = xin
-            if yin < aymin: aymin = yin
-            if yin > aymax and yin < 2200: aymax = yin
-            x=range_map(xin, axmin, axmax, -127, 127)
-            y=range_map(yin, aymin, aymax, -127, 127) * -1
-            gp.move_joysticks(x,y)
-            print("axmin", axmin, "ax", xin, "axmax", axmax, "aymin", aymin, "ay", yin, "aymax", aymax, "x", x, "y", y)
-            sleep(0.01)
-        except:
-            print("ax", xin, "ay", yin)
-            pass
+        ch2in = ch2[0] 
+        if(debug): print("ch2 was ", ch2in)
+    else: 
+        if(debug): print("ch2 was empty")
+        ch2in = 1500
+
+#    and xin > allowed_min and xin < allowed_max and yin > allowed_min and yin < allowed_max: 
+    try:
+        if ch1in < ch1min: ch1min = ch1in
+        if ch1in > ch1max and ch1in < 2200: ch1max = ch1in
+        if ch2in < ch2min: ch2min = ch2in
+        if ch2in > ch2max and ch2in < 2200: ch2max = ch2in
+        x=range_map(ch1in, ch1min, ch1max, -127, 127)
+        y=range_map(ch2in, ch2min, ch2max, -127, 127) * -1
+        gp.move_joysticks(x,y)
+        print("ch1min", ch1min, "ch1in", ch1in, "ch1max", ch1max, "ch2min", ch2min, "ch2in", ch2in, "ch2max", ch2max, "joy_x", x, "joy_y", y)
+        # sleep(0.01)
+    except Exception as e:
+        if(debug): print("ax", ch1in, "ay", ch2in, e)
+        pass
+
+    # if(debug): sleep(0.5)
 
     
     
