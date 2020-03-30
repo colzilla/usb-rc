dcc6715b01e06a2feb8c2e94641bed633afe4947
Colin Taylor <colin.taylor@inmarsat.com>
2020-03-29 22:40:04 -0700

    usb_rc_sanwa_ssr.py - *possibly* works with Sanwa RX in SSR mode

---
+++ usb_rc_sanwa_ssr.py
@@ -0,0 +1,92 @@
+import board
+import digitalio
+import pulseio
+from adafruit_hid.gamepad import Gamepad
+from time import sleep
+import sys
+
+startpause = 1
+print("usb-rc - an adapter from RC car receiver to USB-gamepad/joystick. https://github.com/colzilla/usb-rc")
+print("starting in {} sec...".format(startpause))
+sleep(startpause)
+
+debug=True
+shush=False
+
+#create the gampepad object
+gp = Gamepad()
+
+#setup the pulse readers
+ch1 = pulseio.PulseIn(board.A3, 1)
+ch2 = pulseio.PulseIn(board.A4, 1)
+ch3 = pulseio.PulseIn(board.A2, 1)
+
+#used in auto-calibration
+ch1min = 279
+ch1max = 281
+ch2min = 279
+ch2max = 281
+ch3min = 279
+ch3max = 281
+allowed_min = 130
+allowed_max = 480
+loop=0
+
+def range_map(x, in_min, in_max, out_min, out_max):
+    joy_value =  (x - in_min) * (out_max - out_min) // (in_max - in_min) + out_min
+    return joy_value
+
+while True:
+#    if(debug): print("\nloop[{}]".format(loop))
+    loop += 1
+
+    if len(ch1) > 0: 
+        ch1in = ch1[0] 
+#        if(debug): print("ch1 was ", ch1in)
+    else: 
+#        if(debug): print("ch1 was empty")
+        ch1in = 280
+    
+    if len(ch2) > 0: 
+        ch2in = ch2[0] 
+#        if(debug): print("ch2 was ", ch2in)
+    else: 
+#        if(debug): print("ch2 was empty")
+        ch2in = 280
+
+    if len(ch3) > 0: 
+        ch3in = ch3[0] 
+#        if(debug): print("ch3 was ", ch3in)
+    else: 
+#        if(debug): print("ch3 was empty")
+        ch3in = 280
+
+#    and xin > allowed_min and xin < allowed_max and yin > allowed_min and yin < allowed_max: 
+    try:
+        if ch1in < ch1min: ch1min = ch1in
+        if ch1in > ch1max and ch1in < 480: ch1max = ch1in
+        if ch2in < ch2min: ch2min = ch2in
+        if ch2in > ch2max and ch2in < 480: ch2max = ch2in
+        if ch3in < ch3min: ch3min = ch3in
+        if ch3in > ch3max and ch3in < 480: ch3max = ch3in
+
+        x=range_map(ch1in, ch1min, ch1max, -127, 127)
+        y=range_map(ch2in, ch2min, ch2max, -127, 127) * -1
+        z=range_map(ch3in, ch3min, ch3max, -127, 127) * -1
+
+        gp.move_joysticks(x, y, z)
+        if(debug): print("ch1min", ch1min, "ch1in", ch1in, "ch1max", ch1max, "|| ",
+                         "ch2min", ch2min, "ch2in", ch2in, "ch2max", ch2max, "|| ",
+                         "ch3min", ch3min, "ch3in", ch3in, "ch3max", ch3max, 
+                         "joy_x", x, "joy_y", y, "joy_z", z)
+#        if(not shush): print("x", x, ", y", y, ", z", z)
+        sleep(0.005)
+    except Exception as e:
+#        if(debug): print("ax", ch1in, "ay", ch2in, "az", ch3in, e)
+        pass
+
+    # if(debug): sleep(0.5)
+
+    
+    
+
